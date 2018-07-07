da7f753de628b985c2267d6ef595999eac3894fd
lmbcosta <lmbcosta@hotmail.com>
2018-07-07 16:17:53 +0100

    Initial Commit

---
+++ TextFieldSequenceFormatterManager/TextFieldSequenceFormatterManager.h
@@ -0,0 +1,19 @@
+//
+//  TextFieldSequenceFormatterManager.h
+//  TextFieldSequenceFormatterManager
+//
+//  Created by Luis  Costa on 07/07/18.
+//  Copyright © 2018 Luis  Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextFieldSequenceFormatterManager.
+FOUNDATION_EXPORT double TextFieldSequenceFormatterManagerVersionNumber;
+
+//! Project version string for TextFieldSequenceFormatterManager.
+FOUNDATION_EXPORT const unsigned char TextFieldSequenceFormatterManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextFieldSequenceFormatterManager/PublicHeader.h>
+
+
